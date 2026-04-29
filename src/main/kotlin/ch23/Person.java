d1a4dc274b9b5bb122df76301ded1ddc0d28fd83
bokang03 <bogang12345@gmail.com>
2026-04-29 14:22:30 +0900

    코틀린 apply, let, also, run, with (스코프 함수)

---
+++ src/main/kotlin/ch23/Person.java
@@ -0,0 +1,27 @@
+package ch23;
+
+class Person {
+    String name;
+    int age;
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Person setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public void printInfo() {
+        System.out.println(name + " is " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person()
+                .setName("Alice")
+                .setAge(30);
+        person.printInfo();
+    }
+}
