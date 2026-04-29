f464e3bf021a80d5d7acb12314cea62af87358d6
bokang03 <bogang12345@gmail.com>
2026-04-29 09:46:41 +0900

    코틀린 if문

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,10 @@ import Person
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val number = 10
-    var message = "Hello, Java!"
 
-    println("Number: ${number}")
-    println("Message: ${message}")
+    println("Hello World!")
+
+    val person = Person(20, "John")
+    person.introduce()
 
 }
