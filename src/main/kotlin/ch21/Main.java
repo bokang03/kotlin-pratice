255fafd1fc1300ab77d39bcfec100bebae23e7aa
bokang03 <bogang12345@gmail.com>
2026-04-29 12:38:15 +0900

    코틀린 엘비스 연산자

---
+++ src/main/kotlin/ch21/Main.java
@@ -0,0 +1,10 @@
+package ch21;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = null;
+        String displayName = (name != null) ? name : "Guest";
+
+        System.out.println("Hello, " + displayName);
+    }
+}
