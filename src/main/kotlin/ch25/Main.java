c92ac7dc23cd0ab4a0bbe698d6b5c61fb5d6e340
bokang03 <bogang12345@gmail.com>
2026-04-29 14:27:46 +0900

    코틀린 let

---
+++ src/main/kotlin/ch25/Main.java
@@ -0,0 +1,12 @@
+package ch25;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = null;
+
+        if (name != null) {
+            int length = name.length();
+            System.out.println("Length: " + length);
+        }
+    }
+}
