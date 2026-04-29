eb84f0955f395644c06b095dd8fd28808de655e8
bokang03 <bogang12345@gmail.com>
2026-04-29 12:32:02 +0900

    코틀린 forEach와 Map

---
+++ src/main/kotlin/ch20/Main.java
@@ -0,0 +1,17 @@
+package ch20;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        List<String> greetings = names.stream()
+                .map(name -> "Hello, " + name)
+                .collect(Collectors.toList());
+
+        greetings.forEach(System.out::println);
+    }
+}
