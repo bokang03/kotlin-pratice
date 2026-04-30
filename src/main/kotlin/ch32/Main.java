22ef7afae038610248dd1d1525b9f8021889427b
bokang03 <bogang12345@gmail.com>
2026-04-30 09:08:04 +0900

    코틀린 동반 객체 - 정적 멤버

---
+++ src/main/kotlin/ch32/Main.java
@@ -0,0 +1,16 @@
+package ch32;
+
+class MathUtils {
+    public static final double PI = 3.14159;
+
+    public static int square(int x) {
+        return x * x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PI: " + MathUtils.PI);
+        System.out.println("Square of 4: " + MathUtils.square(4));
+    }
+}
